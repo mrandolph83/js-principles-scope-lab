704951022e2302e6fb4748fb7751b8cf9f361bf8
mrandolph83 <mrandolph83@gmail.com>
2021-07-15 19:31:53 -0400

    commit tree e071d28882b46a121d3e84357271b4e3de518891

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+const leastFavoriteCustomer = "matt";
+
+function upperCaseCustomerName(){
+customerName = customerName.toUpperCase();
+return customerName;
+
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer;
+    
+    };
+
+    function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+    }
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'steve';
+}
